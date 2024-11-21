c8f091bb7f094c223d0054ef8a30a331f68b72b4
audreylearns <audreyduzon.cda@gmail.com>
2024-11-21 18:07:54 -0500

    Initalized basic filtering

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "filters.h" //move all methods here to run
+void filterToRed(cv::Mat& image) {
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
+            //[0] : blue, //[1] : green, //[2] : red,  
+            pixel[0] = 0; // BLUE
+            pixel[1] = 0; // green
+            //pixel[2] = 0; // red
+
+            // //v2
+            // std::cout << "Hello, World!" << std::endl;
+            //max val at 255, min at 0
+        }
+    }
+}
+
+void filterToBlue(cv::Mat& image) {
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
+            //[0] : blue, //[1] : green, //[2] : red,  
+            //pixel[0] = 0; // BLUE
+            pixel[1] = 0; // green
+            pixel[2] = 0; // red
+        }
+    }
+}
+
+void filterToGreen(cv::Mat& image) {
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
+            //[0] : blue, //[1] : green, //[2] : red,  
+            pixel[0] = 0; // BLUE
+            //pixel[1] = 0; // green
+            pixel[2] = 0; // red
+        }
+    }
+}
+
+
+void imageResize(cv::Mat image, cv::Mat& resizedImage){
+    int windowWidth = 800;  // Desired width of the window
+    int windowHeight = 600; // Desired height of the window
+
+    // Calculate the aspect ratio of the original image
+    double aspectRatio = static_cast<double>(image.cols) / static_cast<double>(image.rows);
+
+    // Calculate new dimensions while maintaining the aspect ratio
+    int newWidth, newHeight;
+    if (aspectRatio > 1) { // Image is wider than it is tall
+        newWidth = windowWidth;
+        newHeight = static_cast<int>(windowWidth / aspectRatio);
+    } else { // Image is taller than it is wide
+        newHeight = windowHeight;
+        newWidth = static_cast<int>(windowHeight * aspectRatio);
+    }
+
+    // Resize the image to fit the window
+    
+    cv::resize(image, resizedImage, cv::Size(newWidth, newHeight));
+
+}
+
+int main(){
+
+
+    std::string imagePath = "tifa.jpeg";
+    //Mat is a structure that keeps matrix/image characteristics
+    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+
+    //cv::imshow("Original", image);
+    cv::Mat original_resized; //for resized
+    imageResize(image, original_resized);
+    cv::imshow("original resized", original_resized);
+
+    filterToRed(image);
+
+    std::string outputPath = "img_alt.jpeg"; 
+    cv::imwrite(outputPath, image); //saving to new file
+    
+    std::cout << "Filtered image saved as: " << outputPath << std::endl;
+    cv::Mat alt_resized = cv::imread(outputPath, cv::IMREAD_COLOR);
+    imageResize(alt_resized, alt_resized);
+    cv::imshow("Altered", alt_resized);
+
+
+    cv::waitKey(0);
+    return 0;
+}
